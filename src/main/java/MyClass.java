9697b6537ef05367ebabba608f201c488f291de9
aishagonjobe <alixx379@live.com>
2021-11-25 10:07:50 -0600

    myClass file added

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,25 @@
+public class MyClass {
+//   Java - object oriented programming
+//
+//   An object is an instance of a class
+//
+//   Class is a template of an object -
+//   a blueprint with properties and methods
+//
+//   Procedural Programming
+        public static void main(String args[]) {
+            int studentScore = 100;
+
+            if (studentScore > 90){
+                System.out.println("Student recieved an A.");
+            }else if (studentScore > 80){
+                System.out.println("Student recieved an B.");
+            }else if (studentScore > 70){
+                System.out.println("Student recieved an C.");
+            }else if (studentScore > 60){
+                System.out.println("Student recieved an D.");
+            }else{
+                System.out.println("Student recieved an F.");
+            };
+        }
+}
